5c4f8fb14a9a79bd0ebc47262bdf4fffbc1b037d
Vincent Toscas <vincent@MacBook-Pro-de-george.local>
2020-08-21 15:52:59 +0200

    Work in progress

--- string_machine_1_0.py
+++ string_machine_1_0.py
@@ -10,13 +10,11 @@ user_data = input("Just smash your keyboard !\n")
 
 def cls():
     os.system('cls' if os.name == 'nt' else 'clear')
-
-
 cls()
 
 
 x = 0
-final_output = 0
+
 cool_list = []
 unic_char_list = []
 rep_char = {}
@@ -27,7 +25,7 @@ char_list[:] = user_data
 maximum = math.factorial(len(char_list))
 rep = dict(Counter(char_list))
 
-print("--------------------------- Total of repetitions --------------------------- ")
+
 
 while x < len(char_list):
     print("Occurence of", char_list[x], "is %s" % rep.get(char_list[x]))
@@ -38,15 +36,15 @@ while x < len(char_list):
             rep_char = {char_list[x]: rep.get(char_list[x])}
             foo.update(rep_char)
     x += 1
-print("Unique char : ", unic_char_list)
-print("Characters repeated more than once following the synthax char:occurence = ", foo)
+
 for k in foo.keys():
     maximum //= math.factorial(foo[k])
-print("Combinaisons with the given characters =  ", maximum)
 
 
 
+final_output = 0
 while i < maximum:
+    final_output = 0
     words = ""
     random.shuffle(char_list)
     words = ''.join(char_list)
@@ -56,5 +54,9 @@ while i < maximum:
         cool_list.append(words)
         print(cool_list[final_output])
         i += 1
-    final_output += 1 
+    final_output += 1
 
+print("--------------------------- Total of repetitions --------------------------- ")
+print("Unique char : ", unic_char_list)
+print("Characters repeated more than once following the synthax char:occurence = ", foo)
+print("Combinaisons with the given characters =  ", maximum)
