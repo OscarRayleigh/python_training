2fa089310b0c3982c048b3199259cfdc6ad68cdf
Vincent Toscas <vincent@MacBook-Pro-de-george.local>
2020-08-25 23:34:45 +0200

    Almost done cows and bulls

--- cows_and_bulls.py
+++ cows_and_bulls.py
@@ -1,15 +1,14 @@
 # Create a program that will play the “cows and bulls” game with the user. The game works like this:
 # Randomly generate a 4-digit number.
-# Ask the user to guess a 4-digit number.
+# Ask the user to user_digits a 4-digit number.
 # For every digit that the user guessed correctly in the
 # correct place, they have a “cow”. For every digit the
 # user guessed correctly in the wrong place is a “bull.”
-# Every time the user makes a guess, tell them how
+# Every time the user makes a user_digits, tell them how
 # many “cows” and “bulls” they have. Once the user guesses
 # the correct number, the game is over. Keep track of the
 # number of guesses the user makes throughout the game and
 # tell the user at the end.
-
 import random
 
 def verify_entry():
@@ -23,13 +22,20 @@ def verify_entry():
     return user_digits
 
 def compare_digits(user_digits, solution):
-    print("Bulls = ", len(set(list(solution)) & set(list(user_digits))) )
+    bulls = 0
+    cows = 0
+    for x in range(0,4):
+        if user_digits[x] == solution[x]:
+            cows += 1
+        elif user_digits[x] in solution:
+            bulls += 1
+    print("Bulls = ", bulls)
+    print("Cows = ", cows)
 
 def cows_and_bulls():
     user_digits = verify_entry()
-    solution =  "4043" #format(random.randint(0000,9999), '04d')
+    solution = "5641"#format(random.randint(0000,9999), '04d')
     print("Solution = ", solution)
     print("Entry = ", user_digits)
     compare_digits(user_digits, solution)
-
 cows_and_bulls()
