f78b571d9cdf197d982a586fdc4ce3467f08ec33
Vincent Toscas <vince@iMac-de-Vincent.local>
2020-08-28 04:09:35 +0200

    Working but beginnerish

--- login_system/login_system.py
+++ login_system/login_system.py
@@ -1,6 +1,7 @@
 from cryptography.fernet import Fernet
 import ast
 import os.path
+import os
 
 def main():
     print("============ Main Menu ============ \n")
@@ -10,11 +11,13 @@ def main():
     if a == "2":
         login()
     if a == "3":
-        choice = input("Do you want to generate a new key ? y for yes : ")
+        choice = input("Do you want to generate a new key ? Registered accounts will be lost \nPress y continue or anything else to abort : ")
         if choice == "y":
             os.remove("secret.key")
+            os.remove("login.txt")
             generate_key()
-            main()
+            print("Key succefully reset, old accounts are now lost !")
+            exit()
         else:
             print("Back to main menu ... ")
             main()
@@ -22,7 +25,6 @@ def main():
         print("See ya ...")
         exit()
     else:
-        print("Something wrong happened ")
         main()
 
 def create_account():
@@ -30,9 +32,9 @@ def create_account():
     print("============ Creating account ============ \n")
     username = input("Username : ")
     password = input("Password : ")
-    if username_available(username):
+    if username_is_available(username):
         if checker(username, password):
-            f.write(username + ":" + str(encrypt_message(password)) + "\n")
+            f.write(username + ":" + str(encrypt_password(password)) + "\n")
             print("\n Account succefully created ! ")
             f.close()
             main()
@@ -45,18 +47,23 @@ def login():
     print("============Login============ \n")
     usernames = []
     passwords = []
-    with open("login.txt", "r") as f:
-        for line in f:
-            fields = line.strip().split(":")
-            usernames.append(fields[0])
-            passwords.append(fields[1])
+    if os.path.isfile('./login.txt') == False or os.stat("login.txt").st_size == 0:
+        print("ERROR ! : Account data base empty ... ")
+        main()
+    f = open("login.txt", "r")
+    for line in f:
+        fields = line.strip().split(":")
+        usernames.append(fields[0])
+        passwords.append(fields[1])
+    f.close()
     user_login = zip(usernames, passwords)
     login_dict = dict(user_login)
     username = input("Enter username: ")
     password = input("Enter password: ")
-    txt_password = decrypt_message(login_dict[username])
+
 
     if checker(username, password):
+        txt_password = decrypt_password(login_dict[username])
         if username in login_dict.keys() and txt_password.decode() == password:
             print("You just logged in ! Hello %s !!! " % (username))
             exit()
@@ -64,41 +71,37 @@ def login():
             print("Access Denied\n")
             login()
     else:
-        print("Something wrong happened, you wrote char that cant be in your password ")
+        print("Something wrong happened, you wrote char that cannot be in your password ")
 
 def checker(username, password):
-    if "," in username or "," in password or " " in username:
+    if "," in username or "," in password or " " in username or not password or not username:
         return False
     else:
         return True
 
 def generate_key():
-
     key = Fernet.generate_key()
     with open("secret.key", "wb") as key_file:
         key_file.write(key)
 
 def load_key():
-
     return open("secret.key", "rb").read()
 
-def encrypt_message(password):
-
+def encrypt_password(password):
     key = load_key()
-    encoded_message = password.encode()
+    encoded_password = password.encode()
     f = Fernet(key)
-    encrypted_message = f.encrypt(encoded_message)
-    return encrypted_message
+    encrypted_password = f.encrypt(encoded_password)
+    return encrypted_password
 
-def decrypt_message(encrypted_password_str):
-
+def decrypt_password(encrypted_password_str):
     encrypted_password_byte = ast.literal_eval(encrypted_password_str)
     key = load_key()
     f = Fernet(key)
     decrypted_password = f.decrypt(encrypted_password_byte)
     return decrypted_password
 
-def username_available(username):
+def username_is_available(username):
     usernames = []
     with open("login.txt", "r") as f:
         for line in f:
